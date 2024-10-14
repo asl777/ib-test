cffef9976d746fa400a1c0d6fc5fcdea51b3a399
Anna-Stiina Laidna <70900256+annalaid@users.noreply.github.com>
2024-10-14 20:11:50 +0300

    Added the three views

---
+++ src/views/MainLoan.vue
@@ -0,0 +1,48 @@
+
+<template>
+  <div class="content">
+      <div class="hero flex">
+        <div class="hero-text-container">
+          <p class="">Mini loan</p>
+          <h1><i>Take charge</i> of unexpected cost</h1>
+          <p class="">Finance unforeseen expenses with Inbank. A quick and easy application process with an instant credit decision.</p>
+        </div>
+        <div class="hero-image-container">
+          <img src="../assets/hero-image.png" alt="Man with dog in front of the washing machine">
+        </div>
+      </div>
+    <div class="calculator">
+      <div class="flex">
+        <div>
+          <p class="text-big">Calculate <i>your monthly payment</i></p>
+          <p class="text-regular">Estimate your monthly payments based on the chosen loan amount and time period.</p>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+.hero {
+  gap: 24px;
+  padding: 16px 40px 80px 40px;
+  width: 100%;
+  margin: 0 auto;
+}
+.hero-image-container {
+  width: 1109px;
+  height: 851px;
+}
+.hero-image-container img {
+  border-radius: 200px;
+}
+.hero-text-container {
+  background-color: #D9D4A7;
+  width: 592px;
+  padding: 50% 40px 80px 40px;
+  border-radius: 200px;
+  text-align: center;
+}
+</style>
+<script setup>
+</script>
